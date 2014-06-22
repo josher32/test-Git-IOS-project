60f2ab64c88d7d7ac8f940aaa1838bbd48ddf908
h605506 <joshmeador@h605506.local>
2014-06-21 18:43:13 -0700

    added a test class

---
+++ JMMTest.h
@@ -0,0 +1,13 @@
+//
+//  JMMTest.h
+//  Git Test
+//
+//  Created by h605506 on 6/21/14.
+//  Copyright (c) 2014 Josher. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JMMTest : NSObject
+
+@end
---
+++ JMMTest.m
@@ -0,0 +1,13 @@
+//
+//  JMMTest.m
+//  Git Test
+//
+//  Created by h605506 on 6/21/14.
+//  Copyright (c) 2014 Josher. All rights reserved.
+//
+
+#import "JMMTest.h"
+
+@implementation JMMTest
+
+@end
